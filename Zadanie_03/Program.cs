9d56ce99c36aa4e1e505c4117b490de74330008c
Vlad <blackmor143@yandex.ru>
2022-10-26 15:28:31 +0300

    Выполнили 3 задание

---
+++ Zadanie_03/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите номер дня недели (от 1 до 7): ");
+int number = int.Parse(Console.ReadLine());
+
+string[] days = {"нет", "нет", "нет", "нет", "нет", "да", "да"};
+
+if(number > 7)
+{
+Console.WriteLine("В неделе только 7 дней, введите другую цифру");
+}
+else
+{
+Console.WriteLine(days[number]);
+}
